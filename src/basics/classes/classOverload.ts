e6254170d062cfbdc51e741d117a5fd2d4c83d3e
Gregory Niebanck <gniebanc@vt.edu>
2019-03-08 16:26:59 -0500

    overloading and such

--- src/basics/classes/classOverload.ts
+++ src/basics/classes/classOverload.ts
@@ -1,16 +1,29 @@
 export default class Overloader
 {
-    organizationAsString:string;
-    organizationAsOrganizationObjbect:object
+    paramAsAstring:string;
+    paramAsAnObject:object
 
-    constructor()
+    constructor(private something:string)
     {
 
     }
 
-    async overloadedFunction(organizationStringOrObject:string|object)
+    async overloadedFunction(paramAsAStringOrObject:string|object)
     {
-    
-        console.log(typeof organizationStringOrObject);
+        if(typeof paramAsAStringOrObject === "string")
+        {            
+            console.log("this is a string!")
+            this.paramAsAstring = paramAsAStringOrObject;
+        }
+        else if(typeof paramAsAStringOrObject === "object")
+        {
+            console.log("this is an object!")
+            this.paramAsAnObject = paramAsAStringOrObject;
+        }
     }  
+
+    async saysomthing()
+    {
+        console.log(this.something);
+    }
 }
